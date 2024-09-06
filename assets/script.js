1ee0fb8c7bb826ed650458f01da6c0c5d9f20a30
dimasfirmanda27@gmail.com <dimasfirmanda27@gmail.com>
2024-09-06 23:47:50 +0700

    Adding styling and new content: 1. using dynamic styling with media queries 2. adding portfolios content and grid styling

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const getButton = document.querySelector('.btn-resume');
+
+getButton.addEventListener('click', (url) => {
+  window.open('https://drive.google.com/file/d/1ZFSNVuZBKGwDUoeKLS1HzPkilm7tcDB_/view', '_blank');
+})
