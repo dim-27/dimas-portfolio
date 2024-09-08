7c93960e2527f4c2343361b3ce1e9b593e14fc6d
dimasfirmanda27@gmail.com <dimasfirmanda27@gmail.com>
2024-09-08 18:58:49 +0700

    add new styling: 1. add hamburger navbar function

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,9 @@ const getButton = document.querySelector('.btn-resume');
 const liveEtch = document.querySelector('.live-etch');
 const liveRPS = document.querySelector('.live-rps');
 const liveCalculate = document.querySelector('.live-calculate');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu')
+const navLink = document.querySelectorAll('.nav-link');
 
 getButton.addEventListener('click', (url) => {
   window.open('https://drive.google.com/file/d/1ZFSNVuZBKGwDUoeKLS1HzPkilm7tcDB_/view', '_blank');
@@ -18,3 +21,15 @@ liveRPS.addEventListener('click', () => {
 liveCalculate.addEventListener('click', () => {
   window.open('https://dim-27.github.io/calculator/', '_blank');
 });
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+navLink.forEach((nav) => {
+  nav.addEventListener('click', () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+})
